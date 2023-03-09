7dbb228cb25befb657c9d72a7e0f4091e7feb80d
Etson <etsond@ufl.edu>
2023-03-08 22:35:55 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// check if user is logged in
+let loggedIn = false; // replace with your logic
+
+// show overlay if user is not logged in
+if (!loggedIn) {
+  document.getElementById("overlay").classList.add("visible");
+}
